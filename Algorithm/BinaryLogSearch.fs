70bcd84c9e744061d1cb02a8744539321da40ba4
Jonathan Leaver <jonathan@jet.com>
2017-02-20 17:52:55 -0500

    automatically terminate sequences at end of topic for slow clusters

--- Algorithm/BinaryLogSearch.fs
+++ Algorithm/BinaryLogSearch.fs
@@ -28,22 +28,30 @@ type BinaryLogSearch(connection:Connection,
                       Position=Some position})
 
         let scan (state, position) =
-            connection.readFrom
-            >> Seq.mapi (fun i (event, index) ->
+            let max =
+                match position.UpperBound - position.Current with
+                | x when x >= int64 Int32.MaxValue -> Int32.MaxValue
+                | x -> int x
+            Position.At position.Current
+            |> connection.readFrom
+            |> Seq.mapi (fun i (event, index) ->
                 if i % 100 = 0 then
-                    onStatus(state, { position with Current = index; LowerBound = index })
+                    onStatus(state, { position with Current=index })
                 (event, index))
-            >> Seq.takeWhile isNotCancelled
-            >> Seq.chooseFst Event.data
-        let seek (state, position) =
-            scan (state, position)
+            |> Seq.take max
+            |> Seq.takeWhile isNotCancelled
+            |> Seq.chooseFst Event.data
+        let seek =
+            scan 
             >> Seq.tryHead
 
         let rec search (position:Position) =
 
             let range = position.UpperBound-position.LowerBound
             if range < startScanAt then
-                scan (Scan, position) (Position.At position.LowerBound)
+                let position = { position with Current=position.LowerBound }
+
+                scan (Scan, position)
                 |> Seq.tryFind (fun (t, at) -> t >= target)
                 |> function
                    | Some(t,at) as x ->
@@ -57,9 +65,9 @@ type BinaryLogSearch(connection:Connection,
                         x
             else
                 let point = (range/2L)+position.LowerBound
-                let position = { position with QueryAt = point }
+                let position = { position with QueryAt=point; Current=point }
 
-                match seek (Seek, position) (Position.At point) with
+                match seek (Seek, position) with
                 | None when isCancelled() ->
                     onStatus(Cancelled,position)
                     None
@@ -67,9 +75,9 @@ type BinaryLogSearch(connection:Connection,
                     onStatus(NotFound,position)
                     None
                 | Some(t, at) when t >= target ->
-                    search { position with UpperBound = point }
+                    search { position with UpperBound=point }
                 | Some(t, at) ->
-                    search { position with LowerBound = point }
+                    search { position with LowerBound=point }
 
         //// start search at the start offset
         state.Position
