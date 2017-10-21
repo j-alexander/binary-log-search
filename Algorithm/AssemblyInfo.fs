515d13a87efceeddb2d2c2eb18f2cc8095781198
Jonathan Leaver <jonathan@jet.com>
2017-10-21 15:58:55 -0400

    update installer

--- Algorithm/AssemblyInfo.fs
+++ Algorithm/AssemblyInfo.fs
@@ -4,9 +4,6 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [<assembly: AssemblyTitle("Binary Log Search Algorithm")>]
 [<assembly: AssemblyDescription("Examine log storage JSON topics with JSONPath timestamps to find offsets for a give value.")>]
 [<assembly: AssemblyConfiguration("")>]
@@ -15,27 +12,8 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyCopyright("Copyright © 2016 Jonathan Leaver")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("39bd2d0f-db6a-422e-859d-7066866d25e4")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.7.0")>]
-[<assembly: AssemblyFileVersion("1.0.7.0")>]
+[<assembly: AssemblyVersion("1.0.8.0")>]
+[<assembly: AssemblyFileVersion("1.0.8.0")>]
 
 do
     ()
--- UI/AssemblyInfo.fs
+++ UI/AssemblyInfo.fs
@@ -4,9 +4,6 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [<assembly: AssemblyTitle("Binary Log Search UI")>]
 [<assembly: AssemblyDescription("Examine log storage JSON topics with JSONPath timestamps to find offsets for a give value.")>]
 [<assembly: AssemblyConfiguration("")>]
@@ -15,8 +12,8 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyCopyright("Copyright © 2016-2017 Jonathan Leaver")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
-[<assembly: AssemblyVersion("1.0.7.0")>]
-[<assembly: AssemblyFileVersion("1.0.7.0")>]
+[<assembly: AssemblyVersion("1.0.8.0")>]
+[<assembly: AssemblyFileVersion("1.0.8.0")>]
 
 do
     ()
